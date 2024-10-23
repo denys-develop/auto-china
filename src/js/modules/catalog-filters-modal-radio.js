20fdac45f91adebcb402e122d0bb8dbc7e7badd7
denys-develop <denys.tsuprenko@yandex.ru>
2024-10-24 00:42:35 +0300

    card auto mobile360 v1.0

--- src/js/modules/catalog-filters-modal-radio.js
+++ src/js/modules/catalog-filters-modal-radio.js
@@ -3,17 +3,18 @@ function catalogFiltersModalRadio() {
         const filters = document.querySelector('.catalog__items-filters-modal');
         const openFiltersButton = document.querySelector('.catalog__items-filters-left');
         const closeFiltersButton = document.querySelector('.catalog-modal-btnClose');
+        const body = document.querySelector('body'); 
 
-        
         openFiltersButton.addEventListener('click', () => {
-            filters.style.display = 'block';    
+            filters.style.display = 'block';
+            body.classList.add('no-scroll');    
         });
 
         closeFiltersButton.addEventListener('click', () => {
-            filters.style.display = 'none';   
+            filters.style.display = 'none';
+            body.classList.remove('no-scroll');  
         });
     });
 }
-
 
 export default catalogFiltersModalRadio;
